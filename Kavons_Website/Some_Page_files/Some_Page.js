5a75fc0b4f55da15e58b9bc5b887e0411e36dcfc
Kavon Farvardin <kavon.farvardin@gmail.com>
2011-07-15 22:08:25 -0400

    fixed the previous commit. removed the weird double folder.

--- Kavons_Website/Some_Page_files/Some_Page.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20110715
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Some_Page_files/Some_PageMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
