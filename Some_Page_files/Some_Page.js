c733f5364d6ba53b1f03867d7b5ce497bc66647a
Kavon Farvardin <kavon.farvardin@gmail.com>
2012-08-11 11:18:15 -0400

    test

--- Some_Page_files/Some_Page.js
+++ Some_Page_files/Some_Page.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20110715
+// Created by iWeb 3.0.4 local-build-20120811
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Some_Page_files/Some_PageMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
